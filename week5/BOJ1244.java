2436dc1a501f60bfd87ea37d7d8d8f36365ead2c
JHLEE325 <ljntiger325@naver.com>
2025-03-04 21:01:03 +0900

    주차 폴더화, 6주차 문제풀이

--- week5/BOJ1244.java
+++ week5/BOJ1244.java
@@ -9,7 +9,7 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int num = Integer.parseInt(st.nextToken())+1;
-		boolean[] onoff = new boolean[num];
+		boolean[] onoff = new boolean[num]; // 스위치 온 오프를 boolean 타입 배열로 선언하여 관리
 
 		st = new StringTokenizer(br.readLine());
 		for (int i = 1; i < num; i++) {
@@ -26,23 +26,23 @@ public class Main {
 			int sex = Integer.parseInt(st.nextToken());
 			int idx = Integer.parseInt(st.nextToken());
 
-			if (sex == 1) {
+			if (sex == 1) { // 성별이 남자인 경우
 				int count = 1;
-				while (idx * count < num) {
-					onoff[idx * count] = !onoff[idx * count];
+				while (idx * count < num) { // 인덱스가 범위를 넘을 때 까지
+					onoff[idx * count] = !onoff[idx * count]; // 곱하면서 스위치를 조작
 					count++;
 				}
 			}
 
-			else {
+			else { // 성별이 여자인 경우 지정한 인덱스의 스위치 기준 대칭인 곳의 스위치를 조작
 				int count = 1;
-				onoff[idx] = !onoff[idx];
+				onoff[idx] = !onoff[idx]; // 지정 위치는 무조건 조작
 				while (true) {
-					if (idx - count >= 1 && idx + count < num) {
-						if (onoff[idx - count] == onoff[idx + count]) {
+					if (idx - count >= 1 && idx + count < num) { // 인덱스 범위 판단
+						if (onoff[idx - count] == onoff[idx + count]) { // 대칭인지 판단하여 스위치 조작
 							onoff[idx - count] = !onoff[idx - count];
 							onoff[idx + count] = !onoff[idx + count];
-						} else {
+						} else { // 대칭이 아닌 경우 break
 							break;
 						}
 					} else {
@@ -56,7 +56,7 @@ public class Main {
 
 		StringBuilder sb = new StringBuilder();
 		int count = 0;
-		for (int i = 1; i < num; i++) {
+		for (int i = 1; i < num; i++) { // 출력 포맷에 맞게 수정
 			if(onoff[i]) {
 				sb.append("1 ");
 			}
